b35d26bfa04b07dbf1938326c4dff1c80587984f
Daniel-bee <daniel123_86@live.com>
2022-04-14 21:23:21 +0300

    mxd list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+mixed list
+"""
+
+
+def sum_mixed_list(mxd_lst: list()) -> float:
+    """
+    type-annotated function sum_mixed_list
+    """
+    sum_: float = 0.0
+    for num in mxd_lst:
+        sum_ += num
+    return sum_
