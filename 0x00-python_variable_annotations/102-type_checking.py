05192ec5988ed9b74522f5aea9602b34d265d5ec
Daniel-bee <daniel123_86@live.com>
2022-04-14 23:11:48 +0300

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,13 +2,20 @@
 """
 Type Checking
 """
-from typing import Sequence, Any, Union
+from typing import Tuple, List
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-    Type Checking
-    """
-    if lst:
-        return lst[0]
-    return None
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Type Checking """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
